00a5183bf70c4d3060ee831d6f01069e9f740c9e
Alberto Chesa Tierz <achesa@maz.es>
2017-10-02 10:09:51 +0200

    Agregar archivos de proyecto.

---
+++ TDD-Katas-project/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Katas_project
+{
+    public class FizzBuzz
+    {
+        public string PrintNumber()
+        {
+            var number = new StringBuilder();
+
+            for (var i = 1; i < 101; i++)
+            {
+                number.Append(IsFizzBuzz(i) ? $" FizzBuzz" : IsFizz(i) ? $" Fizz" : IsBuzz(i) ? $" Buzz" : $" {i}");
+            }
+
+            return number.ToString().Trim();
+        }
+
+        private bool IsFizz(int i)
+        {
+            return i % 3 == 0;
+        }
+
+        private bool IsBuzz(int i)
+        {
+            return i % 5 == 0;
+        }
+
+        private bool IsFizzBuzz(int i)
+        {
+            return IsFizz(i) && IsBuzz(i);
+        }
+    }
+}
---
+++ TestFizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,55 @@
+﻿using TDD_Katas_project;
+using Xunit;
+using FluentAssertions;
+using System;
+
+namespace TestFizzBuzz.UnitTests
+{
+    public class TestFizzBuzz : IClassFixture<ResultFizzFixture>
+    {
+        private readonly FizzBuzz _fizzBuzz;
+        private readonly string _resultFizz;
+        public TestFizzBuzz(ResultFizzFixture fixture)
+        {
+            _fizzBuzz = new FizzBuzz();
+            _resultFizz = fixture.ResultFizz;
+        }
+
+        [Fact]
+        public void CanTestFizz()
+        {
+            _fizzBuzz.PrintNumber().Should().Be(_resultFizz);
+        }
+
+        [Fact]
+        public void CanReturnFizzIfNumberDivisibleByThree()
+        {
+            var actualResult = _fizzBuzz.PrintNumber();
+            actualResult.Should().Contain("Fizz");
+        }
+
+        [Fact]
+        public void CanReturnBuzzIfNumberDivisibleByFive()
+        {
+            var actualResult = _fizzBuzz.PrintNumber();
+            actualResult.Should().Contain("Buzz");
+        }
+
+        [Fact]
+        public void CanReturnFizzBuzzIfNumberDivisibleByThreeAndFive()
+        {
+            var actualResult = _fizzBuzz.PrintNumber();
+            actualResult.Should().Contain("FizzBuzz");
+        }
+    }
+
+    public class ResultFizzFixture : IDisposable
+    {
+        public string ResultFizz { get; set; } = @"1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz";
+
+        public void Dispose()
+        {
+            ResultFizz = null;
+        }
+    }
+}
