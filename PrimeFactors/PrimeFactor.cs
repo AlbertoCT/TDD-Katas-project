eab5d9bc90a27928aec8e4511e0ea17b627d1b81
AlbertoCT <pujatolon@gmail.com>
2018-06-14 13:16:16 +0200

    Prime Factors

---
+++ PrimeFactors/PrimeFactor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class PrimeFactor
+    {
+        public static List<int> Generate(int number)
+        {
+            List<int> primes = new List<int>();
+
+            for (int candidate = 2; number > 1; candidate++)
+            {
+                for (; number % candidate == 0; number = number / candidate)
+                {
+                    primes.Add(candidate);
+                }
+            }
+
+            return primes;
+        }
+    }
+}
