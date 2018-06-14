c89ced9564a4ffd7fd26b9cc727163a0e155deb5
AlbertoCT <pujatolon@gmail.com>
2018-06-14 13:17:41 +0200

    OddEven

---
+++ OddEven/OddEven.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenKata
+{
+    public class OddEven
+    {
+        public string PrintNumbers(int startNumber = 1, int finishNumber = 100)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = startNumber; i < finishNumber + 1; i++)
+            {
+                if (IsEven(i))
+                {
+                    stringBuilder.Append("Even,");
+                }
+                else if (IsPrime(i))
+                {
+                    stringBuilder.Append("Prime,");
+                }
+                else if (IsOdd(i))
+                {
+                    stringBuilder.Append("Odd,");
+                }
+                else
+                {
+                    stringBuilder.Append($"{i},");
+                }
+            }
+            return stringBuilder.ToString().TrimEnd(new char[] { ',' });
+        }
+
+        public bool IsEven(int number)
+        {
+            bool IsEven = false;
+
+            if (number % 2 == 0)
+            {
+                IsEven = true;
+            }
+
+            return IsEven;
+        }
+
+        public bool IsOdd(int number)
+        {
+            bool IsOdd = false;
+
+            if (number % 2 != 0)
+            {
+                IsOdd = true;
+            }
+
+            return IsOdd;
+        }
+
+        public bool IsPrime(int number)
+        {
+            bool IsPrime = false;
+
+            if (IsOdd(number))
+            {
+                IsPrime = true;
+
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        IsPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            return IsPrime;
+        }
+    }
+}
---
+++ OddEvenTest/OddEvenTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using OddEvenKata;
+
+namespace OddEvenTest
+{
+    public class OddEvenTest
+    {
+        [Fact]
+        [Trait("Category", "OddEven")]
+        public void CanGenerateNumbers()
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.PrintNumbers();
+            result.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(66)]
+        [InlineData(100)]
+        [Trait("Category", "OddEven")]
+        public void CanDetermineEvens(int number)
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.IsEven(number);
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(65)]
+        [InlineData(99)]
+        [Trait("Category", "OddEven")]
+        public void CanDetermineNotEvens(int number)
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.IsEven(number);
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(65)]
+        [InlineData(99)]
+        [Trait("Category", "OddEven")]
+        public void CanDetermineOdds(int number)
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.IsOdd(number);
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(66)]
+        [InlineData(100)]
+        [Trait("Category", "OddEven")]
+        public void CanDetermineNotOdds(int number)
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.IsOdd(number);
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(41)]
+        [Trait("Category", "OddEven")]
+        public void CanDeterminePrimes(int number)
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.IsPrime(number);
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(9)]
+        [InlineData(99)]
+        [Trait("Category", "OddEven")]
+        public void CanDetermineNoPrimes(int number)
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.IsPrime(number);
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(1, 100)]
+        [InlineData(2, 200)]
+        [InlineData(99, 9999)]
+        [Trait("Category", "OddEven")]
+        public void CanGenerateNumbersRange(int startNumber, int finishNumber)
+        {
+            OddEven oddEven = new OddEven();
+            var result = oddEven.PrintNumbers(startNumber, finishNumber);
+            result.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
