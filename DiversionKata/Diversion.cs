1caab57340ed059ce1521109fee90d925ef85bfe
AlbertoCT <pujatolon@gmail.com>
2018-06-18 11:57:11 +0200

    Diversion Kata

---
+++ DiversionKata/Diversion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DiversionKata
+{
+    public class Diversion
+    {
+        public static int BinaryNumbersSize(int digit)
+        {
+            var binarys = BinaryNumbersGenerator(digit);
+            var nonAdjacent1s = new List<string>();
+
+            foreach (var binary in binarys)
+            {
+                if (!Regex.IsMatch(binary, @"11"))
+                {
+                    nonAdjacent1s.Add(binary);
+                }
+            }
+
+            return nonAdjacent1s.Count;
+        }
+
+        private static List<string> BinaryNumbersGenerator(int digit)
+        {
+            List<string> binarys = new List<string>();
+            int i = 0;
+            string binary = string.Empty;
+            var maxValue = string.Empty.PadLeft(digit, '1');
+
+            while (binary != maxValue)
+            {
+                binary = Convert.ToString(i, 2).PadLeft(digit, '0');
+                binarys.Add(binary);
+                i++;
+            }
+
+            return binarys;
+        }
+    }
+}
---
+++ DiversionTests/DiversionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace DiversionTests
+{
+    public class Diversion
+    {
+        [Fact]
+        [Trait("Category", "Diversion")]
+        public void CombinatiosForThreeDigitNumbers()
+        {
+            DiversionKata.Diversion.BinaryNumbersSize(3).Should().Be(5);
+        }
+
+        [Fact]
+        [Trait("Category", "Diversion")]
+        public void CombinatiosForFourDigitNumbers()
+        {
+            DiversionKata.Diversion.BinaryNumbersSize(4).Should().Be(8);
+        }
+
+        [Fact]
+        [Trait("Category", "Diversion")]
+        public void CombinatiosForFiveDigitNumbers()
+        {
+            DiversionKata.Diversion.BinaryNumbersSize(5).Should().Be(13);
+        }
+
+        [Fact]
+        [Trait("Category", "Diversion")]
+        public void CombinatiosForTenDigitNumbers()
+        {
+            DiversionKata.Diversion.BinaryNumbersSize(10).Should().Be(144);
+        }
+    }
+}
