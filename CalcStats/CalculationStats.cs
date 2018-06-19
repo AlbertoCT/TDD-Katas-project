7b9f847c076b4d9a416a4d7a2a1949c6eb1bca21
AlbertoCT <pujatolon@gmail.com>
2018-06-19 14:17:14 +0200

    Calc Stats

---
+++ CalcStats/CalculationStats.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcStats
+{
+    public class CalculationStats
+    {
+        public static int CalculateMinimum(List<int> numbers)
+        {
+            return (numbers == null || !numbers.Any()) ? 0 : numbers.OrderBy(x => x).First();
+        }
+
+        public static int CalculateMaximum(List<int> numbers)
+        {
+            return (numbers == null || !numbers.Any()) ? 0 : numbers.OrderByDescending(x => x).First();
+        }
+
+        public static int CalculateNumberOfElements(List<int> numbers)
+        {
+            return numbers == null ? 0 : numbers.Count;
+        }
+
+        public static double CalculateAverage(List<int> numbers)
+        {            
+            return (numbers == null || !numbers.Any()) ? 0 : numbers.Average();
+        }
+    }
+}
---
+++ CalcStatsTest/CalcStatsTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace CalcStatsTest
+{
+    public class CalcStatsTest
+    {
+        private List<int> _numbers;
+
+        public CalcStatsTest()
+        {
+            _numbers = new List<int> { 6, 9, 15, -2, 92, 11 };
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateMinimum()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateMinimum(_numbers);
+            resultado.Should().Be(-2);
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateMinimumVoidList()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateMinimum(new List<int>());
+            resultado.Should().Be(0);
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateMaximum()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateMaximum(_numbers);
+            resultado.Should().Be(92);
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateMaximumVoidList()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateMaximum(new List<int>());
+            resultado.Should().Be(0);
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateNumberOfElements()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateNumberOfElements(_numbers);
+            resultado.Should().Be(6);
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateNumberOfElementsVoidList()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateNumberOfElements(new List<int>());
+            resultado.Should().Be(0);
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateAverage()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateAverage(_numbers);
+            resultado.Should().BeApproximately(21.83333333, 0.1);
+        }
+
+        [Fact]
+        [Trait("Category", "CalcStats")]
+        public void CanCalculateCalculateAverageVoidList()
+        {
+            var resultado = CalcStats.CalculationStats.CalculateAverage(new List<int>());
+            resultado.Should().Be(0);
+        }
+    }
+}
